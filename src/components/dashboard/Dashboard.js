c1b4deb49c5a5bfbec1f63599bd428620e6c4692
Adrian Camacho <adriancamacho18@gmail.com>
2021-09-22 15:24:29 -0700

    add dashboard component to app js file

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react' 
 import Notifications from './Notifications'
+import ProjectList from '../projects/ProjectList'
 
 class Dashboard extends Component {
   render() {
     return (
       <div className="dashboard container">
         <div className="row">
-          <div className="col s12 m6"></div>
+          <div className="col s12 m6">
+            <ProjectList />
+          </div>
           <div className="col s12 m5 offset-m1">
             <Notifications />
           </div>
@@ -15,3 +18,5 @@ class Dashboard extends Component {
     )
   }
 }
+
+export default Dashboard
