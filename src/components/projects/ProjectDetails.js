bf7b6ea59acc36c54f8e71131081c9ff8e3563d1
Adrian Camacho <adriancamacho18@gmail.com>
2021-09-22 16:39:01 -0700

    add projectdetails component and route to app

---
+++ src/components/projects/ProjectDetails.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const ProjectDetails = () => {
+  return (
+    <div className="container section project-details">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">Project Title</span>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi nesciunt facere et quas iusto excepturi reprehenderit quasi eos. Reiciendis illum minus accusamus? Aliquid dolores voluptates maiores hic velit tempore omnis?</p>
+        </div>
+        <div className="card-action grey lighten-4 grey-text">
+          <div>Posted by Adrian Camacho</div>
+          <div>22nd of September, 4:30pm</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectDetails
