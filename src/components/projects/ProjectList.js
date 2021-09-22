a34f4c9211b3f9e3da62e25ef8e3182773978342
Adrian Camacho <adriancamacho18@gmail.com>
2021-09-22 15:55:25 -0700

    add projectsummary component to refactor projectlist component

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,33 +1,13 @@
-import React from 'react' 
+import React from 'react'
+import ProjectSummary from './ProjectSummary'
 
 const ProjectList = () => {
   return (
     <div className="project-list section">
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by Adriansito</p>
-          <p className="grey-text">September 22nd, 3pm</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by Adriansito</p>
-          <p className="grey-text">September 22nd, 3pm</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by Adriansito</p>
-          <p className="grey-text">September 22nd, 3pm</p>
-        </div>
-      </div>
-
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
     </div>
   )
 }
