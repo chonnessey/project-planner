aeed3f4eecd0ab59f006917ef51dcc137773ee40
Adrian Camacho <adriancamacho18@gmail.com>
2021-09-22 16:48:37 -0700

    render id on projectdetails component

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
-const ProjectDetails = () => {
+const ProjectDetails = (props) => {
+  const id = props.match.params.id
   return (
     <div className="container section project-details">
       <div className="card z-depth-0">
         <div className="card-content">
-          <span className="card-title">Project Title</span>
+          <span className="card-title">Project Title - {id}</span>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi nesciunt facere et quas iusto excepturi reprehenderit quasi eos. Reiciendis illum minus accusamus? Aliquid dolores voluptates maiores hic velit tempore omnis?</p>
         </div>
         <div className="card-action grey lighten-4 grey-text">
