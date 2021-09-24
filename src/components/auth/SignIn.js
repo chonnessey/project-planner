7508f6b13df6e85551d439f258263e24e44e08f0
Adrian Camacho <adriancamacho18@gmail.com>
2021-09-24 13:21:18 -0700

    add button to sigin component

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -10,7 +10,15 @@ class SignIn extends Component {
         <form onSubmit={} className="white">
           <h5 className="grey-text text-darken-3">Sign In</h5>
           <div className="input-field">
-            <label htmlFor=""></label>
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" onChange={}/>
+          </div>
+          <div className="input-field">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" onChange={}/>
+          </div>
+          <div className="input-field">
+            <button className="btn pink lighten-1 z-depth-0">Login</button>
           </div>
         </form>
       </div>
