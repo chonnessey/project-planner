d46f62ec665e926a6085f4adde20b133aeb74967
Adrian Camacho <adriancamacho18@gmail.com>
2021-09-28 10:47:36 -0700

    add dummy projects to projectReducer

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react' 
 import Notifications from './Notifications'
 import ProjectList from '../projects/ProjectList'
+import { connect } from 'react-redux'
 
 class Dashboard extends Component {
   render() {
+    console.log(this.props)
     return (
       <div className="dashboard container">
         <div className="row">
@@ -19,4 +21,10 @@ class Dashboard extends Component {
   }
 }
 
-export default Dashboard
+const mapStateToProps = (state) => {
+  return {
+    projects: state.project.projects
+  }
+}
+
+export default connect(mapStateToProps)(Dashboard)
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {}
+const initState = {
+  projects: [
+    {id: '1', title: 'help me plan something', content: 'blah blah blah'},
+    {id: '2', title: 'help me bake a cake', content: 'blah blah blah'},
+    {id: '3', title: 'help me cook rice', content: 'blah blah blah'}
+  ]
+}
 
 const projectReducer = (state = initState, action) => {
   return state
